559a93a735d6949c9c8979a79eaa89f36781b985
Abraham <abrahamnhier97@gmail.com>
2026-02-21 19:18:18 +0300

    set up basic routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,32 @@
-import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
+import { Refine } from "@refinedev/core";
 import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
-
 import routerProvider, {
   DocumentTitleHandler,
   UnsavedChangesNotifier,
 } from "@refinedev/react-router";
-import { BrowserRouter, Route, Routes } from "react-router";
+
+import { BrowserRouter, Route, Routes, Outlet } from "react-router";
+
 import "./App.css";
+
 import { Toaster } from "./components/refine-ui/notification/toaster";
 import { useNotificationProvider } from "./components/refine-ui/notification/use-notification-provider";
 import { ThemeProvider } from "./components/refine-ui/theme/theme-provider";
 import { dataProvider } from "./providers/data";
+import { Layout } from "./components/refine-ui/layout/layout";
+
+import { LayoutDashboard, Users } from "lucide-react";
+
+// Pages
+import Dashboard from "@/pages/dashboard";
+import MembersList from "@/pages/members/list";
+import CreateMember from "@/pages/members/create";
+import MemberDetails from "@/pages/members/show";
 
 function App() {
   return (
     <BrowserRouter>
-      <GitHubBanner />
       <RefineKbarProvider>
         <ThemeProvider>
           <DevtoolsProvider>
@@ -29,15 +39,51 @@ function App() {
                 warnWhenUnsavedChanges: true,
                 projectId: "5kDNde-69tDff-FpkTE6",
               }}
+              resources={[
+                {
+                  name: "dashboard",
+                  list: "/",
+                  meta: {
+                    label: "Dashboard",
+                    icon: <LayoutDashboard />,
+                  },
+                },
+                {
+                  name: "members",
+                  list: "/members",
+                  create: "/members/new",
+                  edit: "/members/:id/edit",
+                  show: "/members/:id",
+                  meta: {
+                    label: "Members",
+                    icon: <Users />,
+                  },
+                },
+              ]}
             >
               <Routes>
-                <Route index element={<WelcomePage />} />
+                <Route
+                  element={
+                    <Layout>
+                      <Outlet />
+                    </Layout>
+                  }
+                >
+                  <Route path="/" element={<Dashboard />} />
+                  <Route path="members">
+                    <Route index element={<MembersList />} />
+                    <Route path="create" element={<CreateMember />} />
+                    <Route path="show/:id" element={<MemberDetails />} />
+                  </Route>
+                </Route>
               </Routes>
+
               <Toaster />
               <RefineKbar />
               <UnsavedChangesNotifier />
               <DocumentTitleHandler />
             </Refine>
+
             <DevtoolsPanel />
           </DevtoolsProvider>
         </ThemeProvider>
